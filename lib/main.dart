feca9b70ddc336e7b98f49db45eac8fe091fa5c3
Samson303 <samson.mulugeta@code.berlin>
2020-09-22 20:29:35 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:clean_auth/home_page.dart';
+import 'package:clean_auth/services/authentication_service.dart';
+import 'package:clean_auth/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+            create: (_) => AuthenticationService(FirebaseAuth.instance)),
+        StreamProvider(
+            create: (context) =>
+                context.read<AuthenticationService>().authStateChanges)
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseuser = context.watch<User>();
+    if (firebaseuser != null) {
+      return HomePage();
+    }
+    return SignInPage();
+  }
+}
